4e41dd57b1f41ef71433c9482d218bdd26fc6638
Viktor <viktor.fonic@gmail.com>
2021-02-21 20:26:20 +0100

    Fix image loading

--- src/common/ScrollToTop/index.js
+++ src/common/ScrollToTop/index.js
@@ -1,20 +1,21 @@
-import SvgIcon from "../../common/SvgIcon";
+import SvgIcon from '../../common/SvgIcon';
+import scrollTop from '../../img/svg/scroll-top.svg'
 
-import * as S from "./styles";
+import * as S from './styles';
 
 const Input = () => {
   const scrollUp = () => {
-    const element = document.getElementById("intro");
+    const element = document.body
     element.scrollIntoView({
-      behavior: "smooth",
-      block: "end",
-      inline: "nearest",
+      behavior: 'smooth',
+      block: 'end',
+      inline: 'nearest',
     });
   };
 
   return (
     <S.Up onClick={scrollUp}>
-      <SvgIcon src="scroll-top.svg" width="26px" height="26px" />
+      <SvgIcon src={scrollTop} width="26px" height="26px" />
     </S.Up>
   );
 };
